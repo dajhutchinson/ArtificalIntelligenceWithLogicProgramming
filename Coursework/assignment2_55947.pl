9f8048451053620b852d04a35d92efbbb98ae749
Dom Hutchinson <domahutch@gmail.com>
2020-03-30 19:56:38 +0100

    Part 1 - Finds routes quickly due to new pruning. TODO - Work on find tasks & fuel

--- Coursework/assignment2_55947.pl
+++ Coursework/assignment2_55947.pl
@@ -3,7 +3,7 @@ candidate_number(55947).
 /*
  *  start.
  *  press "run"
- *  reset_game. join_game(A). reset_game.
+ *  reset_game. join_game(A). reset_game. start_game.
  *  set_prolog_flag(answer_write_options,[max_depth(0)]).
  */
 
@@ -72,18 +72,19 @@ test(New_Agenda):-
 
 % Get possible moves (adjacent positions, which aren't walls & haven't been traversed so far)
 % +Pos +Cost_to_Pos +RPath +Target - NewPos -Total_Cost -Cost_to_Pos -New_RPath
-search_as((Pos,Cost_to_Pos,RPath),Target,NewPos,(Total_Cost,Cost_to_NewPos,New_RPath)):-
+search_as((Pos,Cost_to_Pos,RPath),Target,Current_Agenda,NewPos,(Total_Cost,Cost_to_NewPos,New_RPath)):-
   map_adjacent(Pos,NewPos,empty),
   map_distance(NewPos,Target,Cost),
   \+ memberchk(NewPos,RPath),
   Total_Cost is Cost_to_Pos+Cost,
   Cost_to_NewPos is Cost_to_Pos+1,
+  cheapest_option(Current_Agenda,NewPos,Cost_to_NewPos), % prune if there is already route to this position which is cheaper
   append([Pos],RPath,New_RPath).
 
 % Set all possible moves (adjacent positions, which aren't walls & haven't been traversed so far)
 % -Details -NewPos +Pos +Cost_To_Pos +RPath +Target -NewPos -Details -Ns
-new_setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Target,NewPos,Details),Ns):-
-  setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Target,NewPos,Details),Ns),!.
+new_setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Target,Current_Agenda,NewPos,Details),Ns):-
+  setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Target,Current_Agenda,NewPos,Details),Ns),!.
   % new_setof((Details,N),search_as((p(1,2),1,[p(1,1)]),p(20,20),N,Details),Ns).
 new_setof(_,_,[]). % no posible moves
 
@@ -91,9 +92,9 @@ new_setof(_,_,[]). % no posible moves
 % +Current_Agenda +Target -New_Agenda
 update_agenda(Current_Agenda,Target,New_Agenda):-
   Current_Agenda=[((Total_Cost,Cost_To_Pos,RPath),Pos)|Rest],
-  new_setof((Details,NewPos),search_as((Pos,Cost_To_Pos,RPath),Target,NewPos,Details),New_Moves),
+  new_setof((Details,NewPos),search_as((Pos,Cost_To_Pos,RPath),Target,Current_Agenda,NewPos,Details),New_Moves),
   append(Rest,New_Moves,Unsorted_New_Agenda),
-  sort(Unsorted_New_Agenda,New_Agenda).
+  sort(Unsorted_New_Agenda,New_Agenda). % More agressive prunning
 
 % Task has been completed if first item is agenda moves onto target
 % +Target +Agenda -Cost -RPath
@@ -109,15 +110,43 @@ solve_task_as(Target,Agenda,Cost,RPath):-
   solve_task_as(Target,New_Agenda,Cost,RPath).
   % solve_task_as(p(20,20),[((0,0,[]),p(1,1))],Cost,RPath).
 
+% solve task using A*
+% +Task +Cost -Path
 solve_task_1(Task,Cost,Path):-
-  Task=go(Target),
+  Task=go(Target), % extract target
   my_agent(Agent),
-  query_world(agent_current_position,[Agent,P]),
-  Initial_Agenda=[((0,0,[]),P)],
-  solve_task_as(Target,Initial_Agenda,Cost,RPath),
-  RRPath=[Target|RPath],
-  reverse(RRPath,[_Init|Path]),
-  query_world(agent_do_moves,[Agent,Path]).
+  query_world(agent_current_position,[Agent,Cur_Pos]), % get current position
+  Initial_Agenda=[((0,0,[]),Cur_Pos)], % initial agenda start at current position
+  solve_task_as(Target,Initial_Agenda,Cost,RPath), % solve the task
+  RRPath=[Target|RPath], % Add Target to path
+  reverse(RRPath,[_Init|Path]), % Reverse path & remove starting node
+  query_world(agent_do_moves,[Agent,Path]). % display pay
+
+% finall occurences of New_Pos in Agenda which are cheaper (or equally expensive)
+% test_7([((1,2,[3,4]),p(1,1)),((11,12,[13,14]),p(1,2)),((21,22,[23,24]),p(1,1))],p(1,1),10,X).
+% Agenda_empty
+cheaper_options([],_New_Pos,_New_Cost,[]):-!.
+
+% First item is New_Pos
+% +Agenda +New_Pos +New_Cost (Cost to Pos) -New_Pos_Details
+cheaper_options(Agenda,New_Pos,New_Cost,Prev_Details):-
+  Agenda=[(Details,New_Pos)|Rest],
+  Prev_Details=[Details|More_Details],
+  Details=(_,Prev_Cost,_),
+  Prev_Cost@=<New_Cost,
+  cheaper_options(Rest,New_Pos,New_Cost,More_Details),!.
+
+% New_Pos not in first item
+cheaper_options(Agenda,New_Pos,New_Cost,Prev_Details):-
+  Agenda=[_Item|Rest],
+  cheaper_options(Rest,New_Pos,New_Cost,Prev_Details),!.
+
+% Is New_Cost the best Total_Cost encountered to New_Pos yet?
+% +Agenda +New_Pos +New_Cost
+cheapest_option(Agenda,New_Pos,New_Cost):-
+  cheaper_options(Agenda,New_Pos,New_Cost,Better),
+  length(Better,L),
+  L=0.
 /*
  *  TESTS
  */
@@ -140,3 +169,35 @@ test_5(Target,Agenda,Cost,RPath):-
   length(RPath,L),
   L@>2.
   % test_5(p(1,1),[((5,6,[7,8,9]),p(1,1)),p(2,2)],Cost,RPath).
+
+% detect other routes already in agenda
+% +Agenad +New_Item -Occurences
+test_6(Agenda,New_Item,Occurences):-
+  New_Item=((_Total_Cost,Cost_To_NewPos,_RPath),New_Pos),
+  findall(((_,_,_),New_Pos),Agenda,Occurences).
+
+% finall occurences of New_Pos in Agenda which are cheaper
+% test_7([((1,2,[3,4]),p(1,1)),((11,12,[13,14]),p(1,2)),((21,22,[23,24]),p(1,1))],p(1,1),10,X).
+% Agenda_empty
+test_7([],_New_Pos,_New_Cost,[]):-!.
+
+% First item is New_Pos
+% +Agenda +New_Pos +New_Cost (Cost to Pos) -New_Pos_Details
+test_7(Agenda,New_Pos,New_Cost,Prev_Details):-
+  Agenda=[(Details,New_Pos)|Rest],
+  Prev_Details=[Details|More_Details],
+  Details=(_,Prev_Cost,_),
+  Prev_Cost@=<New_Cost,
+  test_7(Rest,New_Pos,New_Cost,More_Details),!.
+
+% New_Pos not in first item
+test_7(Agenda,New_Pos,New_Cost,Prev_Details):-
+  Agenda=[_Item|Rest],
+  test_7(Rest,New_Pos,New_Cost,Prev_Details),!.
+
+% Is New_Cost the best Total_Cost encountered to New_Pos yet?
+% +Agenda +New_Pos +New_Cost
+test_8(Agenda,New_Pos,New_Cost):-
+  test_7(Agenda,New_Pos,New_Cost,Better),
+  length(Better,L),
+  L=0.
