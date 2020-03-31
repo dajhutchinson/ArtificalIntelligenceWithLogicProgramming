0520ffa64572542bd06c0a6b7f2482845ccba4ef
Dom Hutchinson <domahutch@gmail.com>
2020-03-31 15:52:33 +0100

    Part 1 - Now can go to objects. TODO - refuel.

--- Coursework/assignment2_55947.pl
+++ Coursework/assignment2_55947.pl
@@ -56,70 +56,73 @@ search(F,N,N,1) :- % return an adjacent position which is not occupied (ie valid
  *    RPath = reverse of path to Pos
  */
 
-% Test that agenda builds correctly
-% Runs first 8 steps of search, want to change to run until target is found
-test(New_Agenda):-
-  Initial_Agenda=[((0,0,[]),p(1,1))],
-  Target=p(20,20),
-  update_agenda(Initial_Agenda,Target,New_Agenda1), % Run this recursively until task completed, record path
-  update_agenda(New_Agenda1,Target,New_Agenda2),
-  update_agenda(New_Agenda2,Target,New_Agenda3),
-  update_agenda(New_Agenda3,Target,New_Agenda4),
-  update_agenda(New_Agenda4,Target,New_Agenda5),
-  update_agenda(New_Agenda5,Target,New_Agenda6),
-  update_agenda(New_Agenda6,Target,New_Agenda7),
-  update_agenda(New_Agenda7,Target,New_Agenda).
-
-% Get possible moves (adjacent positions, which aren't walls & haven't been traversed so far)
-% +Pos +Cost_to_Pos +RPath +Target - NewPos -Total_Cost -Cost_to_Pos -New_RPath
-search_as((Pos,Cost_to_Pos,RPath),Target,Current_Agenda,NewPos,(Total_Cost,Cost_to_NewPos,New_RPath)):-
-  map_adjacent(Pos,NewPos,empty),
-  map_distance(NewPos,Target,Cost),
-  \+ memberchk(NewPos,RPath),
-  Total_Cost is Cost_to_Pos+Cost,
+% Get a valid move when doing GO TO task (adjacent positions, which aren't walls & haven't been traversed so far)
+% +Pos +Cost_to_Pos +RPath +Task(go/find) +Current_Agenda -NewPos -Total_Cost -Cost_to_Pos -New_RPath
+search_as((Pos,Cost_to_Pos,RPath),go(Target_Pos),Current_Agenda,NewPos,(Total_Cost,Cost_to_NewPos,New_RPath)):-
+  map_adjacent(Pos,NewPos,empty),       % find valid move (can only move into empty)
+  map_distance(NewPos,Target_Pos,Est_Cost), % estimate distance to end (heuristic value)
+  \+ memberchk(NewPos,RPath),           % check not already been to position on this path
+  Total_Cost is Cost_to_Pos+Est_Cost,   % Update costs
   Cost_to_NewPos is Cost_to_Pos+1,
   cheapest_option(Current_Agenda,NewPos,Cost_to_NewPos), % prune if there is already route to this position which is cheaper
-  append([Pos],RPath,New_RPath).
+  append([Pos],RPath,New_RPath).        % add new position to path
+
+% Get a valid move when doing FIND task
+% difference here is that we cannot estimate distance to goal
+% +Pos +Cost_to_Pos +RPath +Task(go/find) +Current_Agenda -NewPos -Total_Cost -Cost_to_Pos -New_RPath
+search_as((Pos,Cost_to_Pos,RPath),find(Target_Obj),Current_Agenda,NewPos,(Total_Cost,Cost_to_NewPos,New_RPath)):-
+  map_adjacent(Pos,NewPos,empty),   % find valid move (can only move into empty)
+  \+ memberchk(NewPos,RPath),       % check not already been to position on this path
+  Cost_to_NewPos is Cost_to_Pos+1,  % == path length
+  Total_Cost is Cost_to_NewPos+0,   % Cannot estimate distance to goal so == Cost_to_NewPos
+  cheapest_option(Current_Agenda,NewPos,Cost_to_NewPos), % prune if there is already route to this position which is cheaper
+  append([Pos],RPath,New_RPath).    % Add new position to path
 
 % Set all possible moves (adjacent positions, which aren't walls & haven't been traversed so far)
-% -Details -NewPos +Pos +Cost_To_Pos +RPath +Target -NewPos -Details -Ns
-new_setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Target,Current_Agenda,NewPos,Details),Ns):-
-  setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Target,Current_Agenda,NewPos,Details),Ns),!.
-  % new_setof((Details,N),search_as((p(1,2),1,[p(1,1)]),p(20,20),N,Details),Ns).
+% -Details -NewPos +Pos +Cost_To_Pos +RPath +Task +Current_Agenda -NewPos -Details -Ns
+new_setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Task,Current_Agenda,NewPos,Details),Ns):-
+  setof((Details,NewPos),search_as((Pos,Cost_to_Pos,RPath),Task,Current_Agenda,NewPos,Details),Ns),!.
+  % new_setof((Details,N),search_as((p(1,2),1,[p(1,1)]),go(p(20,20)),N,Details),Ns).
 new_setof(_,_,[]). % no posible moves
 
 % Update agenda by looking at first item and adding possible moves
-% +Current_Agenda +Target -New_Agenda
-update_agenda(Current_Agenda,Target,New_Agenda):-
+% +Current_Agenda +Task -New_Agenda
+update_agenda(Current_Agenda,Task,New_Agenda):-
   Current_Agenda=[((Total_Cost,Cost_To_Pos,RPath),Pos)|Rest],
-  new_setof((Details,NewPos),search_as((Pos,Cost_To_Pos,RPath),Target,Current_Agenda,NewPos,Details),New_Moves),
+  new_setof((Details,NewPos),search_as((Pos,Cost_To_Pos,RPath),Task,Current_Agenda,NewPos,Details),New_Moves),
   append(Rest,New_Moves,Unsorted_New_Agenda),
   sort(Unsorted_New_Agenda,New_Agenda). % More agressive prunning
 
-% Task has been completed if first item is agenda moves onto target
+% GO TO Task COMPLETE if first item in agenda moves onto target position
 % +Target +Agenda -Cost -RPath
-solve_task_as(Target,Agenda,Cost,RPath):-
-  Agenda=[(Details,Target)|Rest],
-  Details=(_,Cost,RPath),!.        % Placeholder for path not being too long
+solve_task_as(go(Target_Pos),Agenda,Cost,RPath):-
+  Agenda=[(Details,Target_Pos)|Rest],
+  Details=(_,Cost,RRPath),
+  append([Target_Pos],RRPath,RPath),!.
+
+% FIND OBJECT task COMPLETE if first item in agenda is adjacent to object
+solve_task_as(find(Target_Obj),Agenda,Cost,RPath):-
+  Agenda=[(Details,Pos)|Rest],
+  Details=(_,Cost,RRPath),
+  map_adjacent(Pos,_,Target_Obj),
+  append([Pos],RRPath,RPath),!.
 
 % Task not complete, update agenda with new moves
 % +Target +Agenda -Cost -RPath
 % Cost & RPath are extracted from agenda
-solve_task_as(Target,Agenda,Cost,RPath):-
-  update_agenda(Agenda,Target,New_Agenda),
-  solve_task_as(Target,New_Agenda,Cost,RPath).
+solve_task_as(Task,Agenda,Cost,RPath):-
+  update_agenda(Agenda,Task,New_Agenda),
+  solve_task_as(Task,New_Agenda,Cost,RPath).
   % solve_task_as(p(20,20),[((0,0,[]),p(1,1))],Cost,RPath).
 
 % solve task using A*
-% +Task +Cost -Path
-solve_task_1(Task,Cost,Path):-
-  Task=go(Target), % extract target
+% +Task (go/find) +Cost -Path
+solve_task_1(Task,Cost):-
   my_agent(Agent),
   query_world(agent_current_position,[Agent,Cur_Pos]), % get current position
   Initial_Agenda=[((0,0,[]),Cur_Pos)], % initial agenda start at current position
-  solve_task_as(Target,Initial_Agenda,Cost,RPath), % solve the task
-  RRPath=[Target|RPath], % Add Target to path
-  reverse(RRPath,[_Init|Path]), % Reverse path & remove starting node
+  solve_task_as(Task,Initial_Agenda,Cost,RPath), % solve the task
+  reverse(RPath,[_Init|Path]), % Reverse path & remove starting node
   query_world(agent_do_moves,[Agent,Path]). % display pay
 
 % finall occurences of New_Pos in Agenda which are cheaper (or equally expensive)
